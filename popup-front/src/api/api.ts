898efb7240c6e7059f9e1eaece17981659ea3221
unknown <voronine@ukr.net>
2025-03-31 01:30:54 +0300

    api

--- popup-front/src/api/api.ts
+++ popup-front/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { Setting } from '../types/settings'
 
-const API_URL = 'http://localhost:5000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 export const getSettings = async (): Promise<Setting[]> => {
   const response = await axios.get(`${API_URL}/settings`)
